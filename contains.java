c0a847ddf1ece52393f852673ec8473963be8dff
Charlie Simmons <ttnnkkrr@users.noreply.github.com>
2016-07-28 17:52:14 -0400

    Create contains.java

---
+++ contains.java
@@ -0,0 +1,8 @@
+
+    private static boolean contains(String str) {
+        for (someclass me : someclass.values()) {
+            if (me.getValue().equalsIgnoreCase(str))
+                return true;
+        }
+        return false;
+    }
